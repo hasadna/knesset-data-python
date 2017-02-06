8a72e5976ac24f2bfe192e13e92fbb856c8c0696
Ori Hoch <ori@uumpa.com>
2017-02-06 21:23:48 +0200

    copied knesset-data/python

---
+++ knesset_data/dataservice/monitor/metadata/__init__.py
@@ -0,0 +1,4 @@
+"""Monitors the metadata about the Knesset Dataservice
+
+Contains code related to periodical monitoring of the knesset dataservice
+"""
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='knesset-data',
+    version='1.4.0',
+    description='API for access to available Israeli Parliament (Knesset) data',
+    author='Ori Hoch',
+    author_email='ori@uumpa.com',
+    license='GPLv3',
+    url='https://github.com/hasadna/knesset-data-python',
+    packages=find_packages(exclude=["tests", "test.*"]),
+    install_requires=['beautifulsoup4', 'pyslet', 'requests', 'simplejson', 'pyth',
+                      'python-hebrew-numbers', 'cached-property', 'octohub'],
+)
