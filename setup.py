20ef9b2e444436b91fd9c929e5172f8c405ecd7b
Ori Hoch <ori@uumpa.com>
2017-02-07 15:11:38 +0200

    automatic versioning from github tag name

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import os, sys
+
+if os.getenv("TRAVIS_TAG", "") != "" and os.getenv("TRAVIS_REPO_SLUG", "") == "hasadna/knesset-data-python":
+    # this is a travis build eligible for publishing to pypi
+    # get the version number from the tag name
+    version = os.getenv("TRAVIS_TAG").lstrip("v")
+else:
+    version = "0.0.0"
 
 setup(
     name='knesset-data',
-    version='1.6.0',
+    version=version,
     description='API for access to available Israeli Parliament (Knesset) data',
     author='Ori Hoch',
     author_email='ori@uumpa.com',
