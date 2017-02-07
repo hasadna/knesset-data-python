7814ee09f79342c395c52147ce80b40ca8650b0c
Ori Hoch <ori@uumpa.com>
2017-02-07 17:30:42 +0200

    change dataservice to new urls

--- knesset_data/dataservice/constants.py
+++ knesset_data/dataservice/constants.py
@@ -2,13 +2,13 @@
 # this list is based on the list at the following url under ODATA:
 # http://main.knesset.gov.il/Activity/Info/Pages/Databases.aspx
 SERVICE_URLS = {
-    'laws': "http://online.knesset.gov.il/WsinternetSps/KnessetDataService/LawsData.svc",
-    'bills': "http://online.knesset.gov.il/WsinternetSps/KnessetDataService/BillsData.svc",
-    'final_laws': "http://online.knesset.gov.il/WsinternetSps/KnessetDataService/FinalLawsData.svc",
-    'members': "http://online.knesset.gov.il/WsinternetSps/KnessetDataService/KnessetMembersData.svc",
-    'votes': "http://online.knesset.gov.il/WsinternetSps/KnessetDataService/VotesData.svc",
-    'committees': "http://online.knesset.gov.il/WsinternetSps/KnessetDataService/CommitteeScheduleData.svc",
-    'messages': "http://online.knesset.gov.il/WsinternetSps/KnessetDataService/KnessetMessagesData.svc",
-    'mmm': "http://online.knesset.gov.il/WsinternetSps/KnessetDataService/MMMData.svc",
-    'lobbyists': "http://online.knesset.gov.il/WsinternetSps/KnessetDataService/LobbyistData.svc",
+    'laws': "http://knesset.gov.il/KnessetOdataService/LawsData.svc",
+    'bills': "http://knesset.gov.il/KnessetOdataService/BillsData.svc",
+    'final_laws': "http://knesset.gov.il/KnessetOdataService/FinalLawsData.svc",
+    'members': "http://knesset.gov.il/KnessetOdataService/KnessetMembersData.svc",
+    'votes': "http://knesset.gov.il/KnessetOdataService/VotesData.svc",
+    'committees': "http://knesset.gov.il/KnessetOdataService/CommitteeScheduleData.svc",
+    'messages': "http://knesset.gov.il/KnessetOdataService/KnessetMessagesData.svc",
+    'mmm': "http://knesset.gov.il/KnessetOdataService/MMMData.svc",
+    'lobbyists': "http://knesset.gov.il/KnessetOdataService/LobbyistData.svc",
 }
