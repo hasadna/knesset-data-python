3272042a2778678d593d2f56687a2d63f4f67db8
Ori Hoch <ori@uumpa.com>
2017-04-01 13:22:42 +0300

    fix the run_test scripts

--- knesset_data/dataservice/tests/test_members.py
+++ knesset_data/dataservice/tests/test_members.py
@@ -4,6 +4,7 @@ from knesset_data.dataservice.constants import SERVICE_URLS
 from knesset_data.utils.testutils import data_dependant_test
 
 
+@data_dependant_test()
 class TestMembers(unittest.TestCase):
 
     def test_member(self):
